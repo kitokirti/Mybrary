921c3bec383aee4019d1355a2330c8e037d8ccbc
Kirti Soni <kitokirti94@gmail.com>
2020-08-19 21:32:49 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors')
 
 app.set('view engine','ejs');
 app.set('views',__dirname + '/views');
@@ -16,6 +19,7 @@ app.set('layout', './layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({limit : '10mb', extended:false}));
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useUnifiedTopology: true
@@ -25,5 +29,6 @@ db.on('error',error => console.error(error))
 db.once('open',()=> console.log("Connected to database"));
 
 app.use('/',indexRouter);
+app.use('/authors',authorsRouter);
 
 app.listen(process.env.PORT || 3000);
